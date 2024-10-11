c6c715f4faaab0418e91b5995242221d68a77694
Albert <albert.li1@outlook.com>
2024-10-11 11:49:46 +1100

    Added: CLI entry point, accepts parameters, prints output

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
 // The CLI entry point should live here!
+
+import { reverseSentence } from "./reverse";
+
+const argv = process.argv.slice(2)
+if (argv.length > 0) {
+  const sentence = argv[0][0] !== "-" ? argv[0] : ""  
+  const reverseWord = argv.includes("--word")
+  const reverseLetter = argv.includes("--letter")
+  console.log(reverseSentence(sentence, reverseWord, reverseLetter));
+}
+
+/**
+ * https://nodejs.org/docs/latest/api/process.html#process_process_argv
+ * 
+ * Notes:
+ * Need to consider empty string, and args
+ 
+ * Sample Inputs:
+      npm start -- 
+      npm start -- "" --word
+ */
