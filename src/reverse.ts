496809f4d2232bdf38c974961b86e98ce6d932c0
Albert <albert.li1@outlook.com>
2024-10-11 11:48:36 +1100

    Added: Implementation of reverseSentence

--- src/reverse.ts
+++ src/reverse.ts
@@ -1,7 +1,21 @@
+
+
+// This is a sentence - string 
+// Word - ["This", "is", "a", "sentence"] => ["sentence", "a", "is", "This"]
+// Letter - [['T', 'h', 'i', 's'], [X], [Y], [Z]] => [['s', 'i', 'h', 't'], [X], [Y], [Z]]
+// Both - ["This", "is", "a", "sentence"] => [[Z], [Y], [X], ['s', 'i', 'h', 'T']
+
 export const reverseSentence = (
   sentence: string,
   reverseWords: boolean,
   reverseLetters: boolean
 ): string => {
-  throw new Error("Not implemented yet!");
+  let reverse = sentence.split(" ");
+  if (reverseWords) reverse.reverse();
+  if (reverseLetters) {
+    reverse.map((word, index) => {
+      reverse[index] = word.split("").reverse().join("")
+    })
+  }
+  return reverse.join(" ")
 };
